fd6eee24ee76bdb0c82344698601af50f40fa6e0
Ayush Srivastava <ayushs.1992@gmail.com>
2020-10-11 18:51:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ayush Srivastava</h1>
+      <h2>Personal Portfolio</h2>
+    </div>
+  );
+}
